494564d9160d95332241b786893a94301d78f582
JayZ <110582144+JayZcodeditor@users.noreply.github.com>
2023-05-25 15:14:43 +0700

    s

--- server.js
+++ server.js
@@ -12,26 +12,42 @@ client.connect((err, db) => {
     }
 
     // Get the collection
-    const collection = db.collection("products");
+    const collection = db.collection("users");
 
-    // Insert a document
-    collection.insertOne({
-        name: "Product 1",
-        description: "This is a product",
-        price: 100,
-    });
+    // Check if the form has been submitted
+    if (document.getElementById("name").value && document.getElementById("email").value) {
+        // Check if the username and password are correct
+        const name = document.getElementById("name").value;
+        const email = document.getElementById("email").value;
 
-    // Find all documents
-    collection.find().toArray((err, documents) => {
-        // Check for errors
-        if (err) {
-            console.error(err);
-            return;
-        }
+        // Find the user in the database
+        const query = {
+            name: name,
+            email: email,
+        };
+        const cursor = collection.find(query);
 
-        // Print the documents
-        documents.forEach(document => {
-            console.log(document);
+        // Check if the user was found
+        let user;
+        cursor.next((err, doc) => {
+            if (err) {
+                console.error(err);
+                return;
+            }
+            user = doc;
         });
-    });
+
+        // If the user was found, log them in
+        if (user) {
+            // Set the session variables
+            sessionStorage.setItem("name", user.name);
+            sessionStorage.setItem("email", user.email);
+
+            // Redirect the user to the home page
+            window.location.href = "index.html";
+        } else {
+            // The user was not found, show an error message
+            alert("Invalid name or email address.");
+        }
+    }
 });
